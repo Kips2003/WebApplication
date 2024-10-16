31c000966a11d9453f7a4f94a83461ae81d47fd9
Kips2003 <giorgikiparoidze07@gmail.com>
2024-10-16 04:54:57 +0400

    up

--- WebApi/Services/Address/AddressRepository.cs
+++ WebApi/Services/Address/AddressRepository.cs
@@ -24,16 +24,7 @@ public class AddressRepository : IAddressRepository
     }
     public async Task<IEnumerable<Models.Address>> GetAddressByUserIdAsync(int userId)
     {
-        return await _context.Addresses.Select(a => new Models.Address
-        {
-            Id = a.Id,
-            Street = a.Street,
-            City = a.City,
-            State = a.State,
-            Country = a.Country,
-            PostalCode = a.Country,
-            UserId = a.UserId
-        }).Where(a => a.UserId == userId).ToListAsync();
+        return await _context.Addresses.Where(a => a.UserId == userId).ToListAsync();
     }
 
     public async Task<Models.Address> CreateAddressAsync(Models.Address address)
